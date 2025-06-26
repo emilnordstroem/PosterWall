8446a608dc8bf411a1c8e7024feb5162c0b7b9b9
emilnordstroem <emilstoeve@outlook.com>
2025-06-26 18:39:40 +0200

    Login feature (primitive) for user on site

---
+++ view/Main.js
@@ -0,0 +1,25 @@
+
+
+function login() {
+    const username = document.getElementById("username").value;
+    const date_of_birth = document.getElementById("dateOfBirth").value;
+
+    if (!username || !date_of_birth) {
+        return;
+    }
+
+    sessionStorage.setItem(
+        "logged_in_as",
+        JSON.stringify({ username, date_of_birth })
+    );
+
+    window.location.href = "wall.html";
+}
+
+function wall_page_loaded(){
+    const userData = JSON.parse(sessionStorage.getItem("logged_in_as"));
+    if (userData && userData.username) {
+        document.getElementById("greetingUser").textContent =
+            "Hello, " + userData.username + "!";
+    }
+}
