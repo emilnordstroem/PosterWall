6598b949139fb13dbac2ff48be5f1757a6157562
emilnordstroem <emilstoeve@outlook.com>
2025-10-29 14:35:02 +0100

    Refactor of project architecture. Express dependencies retreived

--- public/Index.js
+++ public/Index.js
@@ -1,28 +1,8 @@
-import { PostController } from './scripts/PostController.js';
-import { isPostContentValid } from './scripts/Validation.js';
 
-const postController = new PostController()
+const app = require('express')()
+const PORT = 8000
 
-function main () {
-    onSubmit('submitPost', onPostSubmit)
-}
-
-function onSubmit (elementID, actionFunction) {
-    const element = document.getElementById(elementID)
-    element.addEventListener('click', actionFunction)
-}
-
-function onPostSubmit () {
-    const postContentElement = document.getElementById('postContent')
-    if (!postContentElement) {
-        return;
-    }
-    const postContent = postContentElement.value.trim()
-    if (!isPostContentValid(postContent)) {
-        return
-    }
-    postController.createPost(postContent, new Date())
-    postContentElement.value = ''
-}
-
-main()
+app.listen(
+    PORT,
+    () => console.log(`It's alive on http://localhost:${PORT}`)
+)
--- public/scripts/Validation.js
+++
@@ -1,6 +0,0 @@
-
-export function isPostContentValid (content) {
-    return content !== null 
-        && typeof content === 'string'
-        && (content.length > 0 && content.length <= 280)
-}
