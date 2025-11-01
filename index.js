8539dfae933789db337e544dc2a917c46c5a384b
emilnordstroem <emilstoeve@outlook.com>
2025-11-01 12:13:03 +0100

    Comments to better document code written

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import path from 'path'
-import { fileURLToPath } from 'url'
+import {fileURLToPath} from 'url'
 
+// pointer (import.meta.url) to current module file (index.js)
+// fileURLToPath function converts url to path
 const __filename = fileURLToPath(import.meta.url)
+// parent folder of current file (/posterwall)
 const __dirname = path.dirname(__filename)
 
 const app = express()
@@ -10,11 +13,11 @@ const port = 10000
 
 // middleware - serves only the client (browser)
 app.use(express.static('assets'))
-
-
+ 
+// GET front page
 app.get('/', async (request, response) => {
     try {
-        const html = path.join(__dirname, 'assets', 'html', 'index.html')
+        const html = path.join(__dirname, 'assets', 'html', 'index.html') //.join combines into readable path
         response.sendFile(html)
     } catch (error) {
         console.error('Error occured: ', error.message)
