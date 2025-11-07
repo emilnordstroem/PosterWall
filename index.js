a42feb02d0704c0d773a703f3adfc86b34e66688
emilnordstroem <emilstoeve@outlook.com>
2025-11-07 21:47:24 +0100

    Rechanges framework to use pug engine instead of component HTML using strings implemented

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+import signUpRender from './views/controller/signUpController.js'
+
+const app = express()
+const port = 10000
+
+app.set('view engine', 'pug')
+
+app.use(morgan('short'))
+app.use(express.static('/assets'))
+
+app.use(bodyParser.urlencoded(
+    {extended: true} // tilader specialtegn (" ", =, %, ø, ^* osv)
+))
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.post('/signup', signUpRender)
+
+app.use((request, response, next) => {
+    response.status(404).send('Unknown URL input')
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
