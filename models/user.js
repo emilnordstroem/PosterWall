ee61869874ac49fa9d6a5be14cfea1311cdfa5a7
emilnordstroem <emilstoeve@outlook.com>
2025-12-02 08:34:01 +0100

    User.js models class constructed - containing necessary properties for login and sign up

---
+++ models/user.js
@@ -0,0 +1,18 @@
+
+class User {
+    static #globalIdentification = 0
+
+    constructor (id, username, password) {
+        if (!id) {
+            this.id = ++User.#globalIdentification
+        } else if (id >= User.#globalIdentification) {
+            this.id = id
+            User.#globalIdentification = ++this.id
+        } else {
+            this.id = id
+        }
+        this.username = username
+        this.password = password
+    }
+
+}
